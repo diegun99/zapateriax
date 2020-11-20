6406b959ab6c05724416fa775a7fd6f8cf5ff4ab
diegun99 <diegun123456789@gmail.com>
2020-11-19 20:36:05 -0500

    incluyendo phps

---
+++ archivos/secciones/footer.php
@@ -0,0 +1,42 @@
+<footer class="pie">
+        <div>
+            <h4>SERVICIO AL CLIENTE</h4>
+            <p>Seguimiento a Pedidos</p>
+            <p>Contáctanos</p>
+            <p>Nuestro compromiso</p>
+        </div>
+        <div>
+            <h4>INFORMACIÓN</h4>
+            <p>Preguntas frecuentes</p>
+            <p>Términos y condiciones</p>
+            <p>Guía de tallas</p>
+        </div>
+        <div>
+            <h4>CORPORATIVO</h4>
+            <p>Quiénes somos</p>
+            <p>Nuestro Calzado</p>
+            <p>Todo sobre los pies</p>
+            <p>Como ser empleado</p>
+            <p>Aviso legal</p>
+        </div>
+    </footer>
+    <div class="copyright">
+        <p>&copy; ING. LEONARDO ALVAREZ</p>
+        <div class="redes">
+            <a href="#">
+                <img src="img/fb.webp" alt="Facebook">
+            </a>
+            <a href="#">
+                <img src="img/twitter.png" alt="Twitter">
+            </a>
+            <a href="#">
+                <img src="img/instagram.png" alt="Instagram">
+            </a>
+            <a href="#">
+                <img src="img/youtube.png" alt="YouTube">
+            </a>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ archivos/secciones/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zapateria X</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <header class="encabezado">
+        <a href="index.php">    
+            <img src="img/logo.png" alt="Logo Zapatería">
+        </a>
+        <h2 class="fw-300">"El mejor calzado al alcance de sus manos"</h2>
+        <nav>
+            <a href="clientes.php">Registrar Clientes</a>
+            <a href="#">Mujeres</a>
+            <a href="#">Hombres</a>
+            <a href="#">Niños</a>
+            <a href="contacto.php">Contacto</a>
+        </nav>
+    </header>
